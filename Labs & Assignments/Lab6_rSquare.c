eaff5a67b788b6d927d048a9fde2959c4a52a018
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-02 11:28:54 +0800

    Lab 6 rSquare

--- Labs & Assignments/Lab6_rSquare.c
+++ Labs & Assignments/Lab6_rSquare.c
@@ -4,21 +4,43 @@ int rSquare1(int num);
 void rSquare2(int num, int *result); 
 
 int main(){ 
-    int number, result=0; 
+   int number, result=0; 
 
-    printf("Enter the number: \n"); 
-    scanf("%d", &number); 
-    printf("rSquare1(): %d\n", rSquare1(number));  
-    rSquare2(number, &result); 
-    printf("rSquare2(): %d\n", result);                 
+   printf("Enter the number: \n"); 
+   scanf("%d", &number); 
+   printf("rSquare1(): %d\n", rSquare1(number));  
+   rSquare2(number, &result); 
+   printf("rSquare2(): %d\n", result);                 
 
-    return 0; 
+   return 0; 
 } 
 
 int rSquare1(int num){ 
-   /* Write your code here */ 
+   // Base case: if num is 0, return 0
+   if (num == 0)
+   return 0;
+
+   // Calculate the current odd number to add
+   // For num=1: 1
+   // For num=2: 1+3
+   // For num=3: 1+3+5
+   // and so on...
+   int oddNum = 2 * num - 1;
+
+   // Recursive case: add current odd number to sum of previous odd numbers
+   return oddNum + rSquare1(num - 1);
 }
 
 void rSquare2(int num, int *result){ 
-   /* Write your code here */ 
+   // Base case: if num is 0, set result to 0
+   if (num == 0) {
+      *result = 0;
+      return;
+   }
+
+   // Calculate sum of odd numbers for num-1
+   rSquare2(num - 1, result);
+
+  // Add the current odd number
+  *result = *result + (2 * num - 1);
 }
