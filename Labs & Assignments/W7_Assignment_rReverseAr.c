b5482facfd32820fa6d7340d87e2090b50212632
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-02 11:47:46 +0800

    Week 7 Assignment rReverseAr

--- Labs & Assignments/W7_Assignment_rReverseAr.c
+++ Labs & Assignments/W7_Assignment_rReverseAr.c
@@ -25,5 +25,15 @@ int main(){
 }
 
 void rReverseAr(int ar[], int size){   
- /* Write your code here */ 
+    // Base case: if array is empty or has only one element
+    if (size <= 1)
+    return;
+
+    // Swap the first and last elements
+    int temp = ar[0];
+    ar[0] = ar[size-1];
+    ar[size-1] = temp;
+
+    // Recursively reverse the rest of the array (excluding first and last elements)
+    rReverseAr(ar + 1, size - 2);
 } 
