ffeacf3d0ff6f86eca670715e873f144aadb1111
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-02 11:40:26 +0800

    Week 7 Assignment rExtEvenDigits

--- Labs & Assignments/W7_Assignment_rExtEvenDigits.c
+++ Labs & Assignments/W7_Assignment_rExtEvenDigits.c
@@ -13,5 +13,31 @@ int main(){
 } 
 
 void rExtEvenDigits(int num, int *evenPtr){
-   /* Write your code here */ 
+    // Initialize for the first call
+    static int isFirstCall = 1;
+    if (isFirstCall) {
+        *evenPtr = -1;  // Start with -1 to indicate no even digits found yet
+        isFirstCall = 0;
+    }
+    
+    // Base case
+    if (num == 0) {
+        isFirstCall = 1;  // Reset for next time this function is called
+        return;
+    }
+    
+    // Get the current digit
+    int digit = num % 10;
+    
+    // Recursive call to process the rest of the number
+    rExtEvenDigits(num / 10, evenPtr);
+    
+    // After processing left portion, check if current digit is even
+    if (digit % 2 == 0) {
+        if (*evenPtr == -1) {
+            *evenPtr = digit;  // First even digit
+        } else {
+            *evenPtr = *evenPtr * 10 + digit;  // Add to existing even digits
+        }
+    }
 }
