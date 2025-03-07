04322190eb94eaa161019e464472bfbdc3cb58c0
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-07 15:14:14 +0800

    Mock Test pattern1

--- Mock Test/pattern1.c
+++ Mock Test/pattern1.c
@@ -8,8 +8,8 @@ int main(){
 
     printf("The pattern is: \n");
 
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j < height - i - 1; j++)
+    for (int i = 0; i <= height; i++){
+        for (int j = 0; j < (height - i); j++)
             printf(" ");
         for (int j = 0; j < 2 * i - 1; j++)
             printf("*");
