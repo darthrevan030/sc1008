fedf415a7fbf4e9ccbeadce84dfe590ca2e8f98d
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-11 20:47:00 +0800

    Binary to Decimal Attempt 1

--- Mock Test/binaryToDec.c
+++ Mock Test/binaryToDec.c
@@ -2,6 +2,27 @@
 #include <math.h> 
 
 int main(){
+    int bin;
+    int decVal = 0;
 
+    int base = 1;
+    int temp = bin;
+
+    printf("Enter a binary number: \n");
+    scanf("%d", bin);
+
+    while(temp){
+        int last_digit = temp % 10;
+
+        temp = temp / 10;
+
+        decVal += last_digit * base;
+
+        base = base * 2;
+    }
+    
+
+    printf("The equivalent decimal number: %d", decVal);
     return 0;
+
 }
