7a37b2e227c4b0716b15be6d68b0e813b2f9089a
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-12 19:45:06 +0800

    Redid Lab 1 printAverage

--- C/_Redo for Practice/Labs/Lab 1/Lab1_printAverage.c
+++ C/_Redo for Practice/Labs/Lab 1/Lab1_printAverage.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
 int main() {
+    int lines;
+
+    printf("Enter number of lines: \n");
+    scanf("%d", &lines);
     
-    
+    for (int i = 0; i <= lines; i++){
+        int num;
+        int sum = 0;
+        int count = 0;
+
+        printf("Enter line %d (end with -1): \n", i);
+
+        while(1){
+            scanf("%d", &num);
+
+            if (num == -1){
+                break;
+            }
+
+            sum += num;
+            count++;
+        }
+        
+        double avg = (double)sum / count;
+        printf("Average = %.2lf\n", avg);
+
+        return 0;
+
+    }
+
     return 0;
 }
