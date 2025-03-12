d82c7354381da212e21c4803f13cb2e4a584bbe0
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-12 21:40:07 +0800

    Redid Lab 2 numDigits

--- C/_Redo for Practice/Labs/Lab 2/Lab2_ numDigits.c
+++ C/_Redo for Practice/Labs/Lab 2/Lab2_ numDigits.c
@@ -16,9 +16,29 @@ int main()
 }
 
 int numDigits1(int num) {
-    
+    int count = 0;
+
+    if (num == 0){
+        return 1;
+    }
+
+    while (num != 0){
+        count++;
+        num /= 10;
+    }
+
+    return count;
 }
 
 void numDigits2(int num, int *result) {
-    
+    *result = 0;
+
+    if (num == 0){
+        return 1;
+    }
+
+    while (num != 0){
+        (*result)++;
+        num /= 10;
+    }
 }
