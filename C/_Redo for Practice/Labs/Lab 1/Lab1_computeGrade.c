da0cd72f143f92ac121935e02b2ffa9efdff6327
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-12 19:44:41 +0800

    Redo of Lab 1 computeGrade

--- C/_Redo for Practice/Labs/Lab 1/Lab1_computeGrade.c
+++ C/_Redo for Practice/Labs/Lab 1/Lab1_computeGrade.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    
+    int marks;
+    int studentID;
+
+
+    while(1){
+        printf("Enter Student ID: \n");
+        scanf("%d", &studentID);
+
+        if(studentID == -1){
+            break;
+        }
+
+        printf("Enter Mark: \n");
+        scanf("%d", &marks);
+
+        if(75 <= marks && marks <= 100){
+            printf("Grade = A\n");
+        } else if(65 <= marks && marks <= 74){
+            printf("Grade = B\n");
+        } else if(55 <= marks && marks <= 64){
+            printf("Grade = C\n");
+        } else if(45 <= marks && marks <= 54){
+            printf("Grade = D\n");
+        } else if(0 <= marks && marks <= 44){
+            printf("Grade = F\n");
+        }
+    }
     
     return 0;
 }
