1d8d6aef1177c7401d26e763388eed79389bcbab
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-12 21:48:09 +0800

    Redid Lab 2 digiPos

--- C/_Redo for Practice/Labs/Lab 2/Lab2_digitPos.c
+++ C/_Redo for Practice/Labs/Lab 2/Lab2_digitPos.c
@@ -5,7 +5,7 @@ void digitPos2(int num, int digit, int *result);
 
 int main(){ 
     int number, digit, result=0; 
- 
+
     printf("Enter the number: \n"); 
     scanf("%d", &number); 
     printf("Enter the digit: \n"); 
@@ -17,10 +17,27 @@ int main(){
 }
 
 int digitPos1(int num, int digit) {
-    
+    int position = 1;
+
+    while (num > 0){
+        if (num % 10 == digit){
+            return position;
+        }
+        position++;
+        num /= 10;
+    }
     return 0;
 }
 
 void digitPos2(int num, int digit, int *result) {
-    
+    *result = 0;
+    int position = 1;
+
+    while (num > 0){
+        if (num % 10 == digit){
+            *result = position;
+        }
+        position++;
+        num /= 10;
+    }
 }
