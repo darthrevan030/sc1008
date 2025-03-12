d816c76fd4aaf508c7977f021740a27e12b14dcf
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-12 21:49:11 +0800

    Added Main function

--- C/Labs/Lab 2/Lab2_square.c
+++ C/Labs/Lab 2/Lab2_square.c
@@ -1,3 +1,19 @@
+#include <stdio.h> 
+
+int square1(int num); 
+void square2(int num, int *result); 
+
+int main(){ 
+    int number, result=0; 
+
+    printf("Enter the number: \n"); 
+    scanf("%d", &number); 
+    printf("square1(): %d\n", square1(number));  
+    square2(number, &result); 
+    printf("square2(): %d\n", result);                 
+    return 0; 
+}
+
 int square1(int num) {
     int sum = 0;
     int odd = 1;
