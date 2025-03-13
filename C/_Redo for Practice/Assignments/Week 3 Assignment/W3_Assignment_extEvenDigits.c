fed1ee4e93d15d3c20fbc672d42b26c163d35fb1
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-14 01:14:42 +0800

    Redid Week 3 Assignment extEvenDigits

--- C/_Redo for Practice/Assignments/Week 3 Assignment/W3_Assignment_extEvenDigits.c
+++ C/_Redo for Practice/Assignments/Week 3 Assignment/W3_Assignment_extEvenDigits.c
@@ -1,3 +1,21 @@
+#include <stdio.h> 
+#define INIT_VALUE 999 
+
+int extEvenDigits1(int num); 
+void extEvenDigits2(int num, int *result); 
+
+int main(){
+    int number, result = INIT_VALUE; 
+    
+    printf("Enter a number: \n"); 
+    scanf("%d", &number); 
+    printf("extEvenDigits1(): %d\n", extEvenDigits1(number));         
+    extEvenDigits2(number, &result); 
+    printf("extEvenDigits2(): %d\n", result); 
+    
+    return 0; 
+}
+
 int extEvenDigits1(int num) {
     int result = 0;
     int multiplier = 1;
@@ -17,3 +35,24 @@ int extEvenDigits1(int num) {
 }
 
 void extEvenDigits2(int num, int *result) {
+
+    *result = 0;
+    int multiplier = 1;
+    int hasEven = 0;
+
+    while (num > 0){
+        int digit = num % 10;
+        
+        if (digit % 2 == 0){
+            *result += (digit * multiplier);
+            multiplier *= 10;
+            hasEven = 1;
+        }
+        num /= 10;
+    }
+
+    if (!hasEven){
+        *result = -1;
+    }
+
+}
