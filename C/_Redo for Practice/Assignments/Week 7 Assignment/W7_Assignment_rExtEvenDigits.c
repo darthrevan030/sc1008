a00a4a3aa83e1ff81e173236c96ccb5c989be0ee
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-14 11:06:03 +0800

    Redid Week 7 Assignment rExtEvenDigits

--- C/_Redo for Practice/Assignments/Week 7 Assignment/W7_Assignment_rExtEvenDigits.c
+++ C/_Redo for Practice/Assignments/Week 7 Assignment/W7_Assignment_rExtEvenDigits.c
@@ -14,4 +14,26 @@ int main(){
 
 void rExtEvenDigits(int num, int *evenPtr){
 
+    static int firstcall = 1;
+    if (firstcall){
+        *evenPtr = -1;
+        firstcall = 0;
+    }
+
+    if (num == 0){
+        firstcall = 1;
+        return;
+    }
+
+    int lastDigit = num % 10;
+
+    rExtEvenDigits(num / 10, evenPtr);
+
+    if (lastDigit % 2 == 0) {
+        if (*evenPtr == -1){
+            *evenPtr = lastDigit;
+        } else {
+        *evenPtr = *evenPtr * 10 + lastDigit;
+        }
+    }
 }
