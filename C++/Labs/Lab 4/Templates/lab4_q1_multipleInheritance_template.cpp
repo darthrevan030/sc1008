a6554e22069b671b5f9c3db548065bd72fa1ab39
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-09 20:56:05 +0800

    Lab 4 Q1 Template

---
+++ C++/Labs/Lab 4/Templates/lab4_q1_multipleInheritance_template.cpp
@@ -0,0 +1,61 @@
+///////// Student Info/////////
+//
+//           Your Name:__________
+//      Your NTU Email:__________
+//
+//
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Base class: Sports
+class Sports {
+protected:
+    string sport; // sport name
+public:
+    // TO-DO 1: Implement the constructor and descructor
+    //
+    //
+
+    // Display sports information
+    void displaySports() const {
+        cout << "Sport: " << sport << endl;
+    }
+};
+
+// Base class: Academics
+class Academics {
+protected:
+    float gpa; // GPA
+public:
+    // TO-DO 2: Implement the constructor and destructor
+    //
+    //
+
+
+    // Display academic information
+    void displayAcademics() const {
+        cout << "GPA: " << gpa << endl;
+    }
+};
+
+// TO-DO 3: Implement the Derived class: StudentAthlete, which inherits from both Academics and Sports
+
+
+int main() {
+    //Create a StudentAthlete instance
+    StudentAthlete stu("Basketball", 3.8, "Bob", 21, 1001);
+
+    // Display all the information
+    cout<<endl;
+    stu.displayInfo();
+    cout<<endl;
+    
+    // Test code to demonstrate multiple inheritances
+    stu.displaySports();
+    stu.displayAcademics();
+    cout<<endl;
+    
+    return 0;
+}
