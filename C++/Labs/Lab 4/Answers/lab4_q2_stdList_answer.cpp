406e5f17848caedbbb65f32303572df54650630d
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-09 20:57:49 +0800

    Update student information in lab4_q2_stdList_answer.cpp

--- C++/Labs/Lab 4/Answers/lab4_q2_stdList_answer.cpp
+++ C++/Labs/Lab 4/Answers/lab4_q2_stdList_answer.cpp
@@ -1,7 +1,8 @@
 ///////// Student Info/////////
 //
-//           Your Name:__________
-//      Your NTU Email:__________
+//           Your Name: Samarth Bhatia
+//      Your NTU Email: samarth009@e.ntu.edu.sg
+//
 //
 //
 //
