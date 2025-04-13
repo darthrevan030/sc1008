80715a66d9ac3ac037c6508211b46191c8fe61bf
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-13 15:27:54 +0800

    Lab 4 Q1 Answer

--- C++/Labs/Lab 4/Answers/lab4_q1_multipleInheritance_answer.cpp
+++ C++/Labs/Lab 4/Answers/lab4_q1_multipleInheritance_answer.cpp
@@ -16,8 +16,16 @@ protected:
     string sport; // sport name
 public:
     // TO-DO 1: Implement the constructor and descructor
-    //
-    //
+    
+    // Constructor
+    Sports(string s) : sport(s) {
+        cout << "Sports constructor!" << endl;
+    }
+
+    // Destructor
+    ~Sports() {
+        cout << "Sports destructor!" << endl;
+    }
 
     // Display sports information
     void displaySports() const {
@@ -31,9 +39,16 @@ protected:
     float gpa; // GPA
 public:
     // TO-DO 2: Implement the constructor and destructor
-    //
-    //
 
+    // Constructor
+    Academics(float g) : gpa(g) {
+        cout << "Academics constructor!" << endl;
+    }
+    
+    // Destructor
+    ~Academics() {
+        cout << "Academics destructor!" << endl;
+    }
 
     // Display academic information
     void displayAcademics() const {
@@ -42,7 +57,31 @@ public:
 };
 
 // TO-DO 3: Implement the Derived class: StudentAthlete, which inherits from both Academics and Sports
-
+class StudentAthlete : public Sports, public Academics {
+    private:
+        string name;
+        int age;
+        int studentID;
+        
+    public:
+        // Constructor
+        StudentAthlete(string s, float g, string n, int a, int id)
+            : Sports(s), Academics(g), name(n), age(a), studentID(id) {
+            cout << "StudentAthlete constructor!" << endl;
+        }
+        
+        // Destructor
+        ~StudentAthlete() {
+            cout << "StudentAthlete destructor!" << endl;
+        }
+        
+        // Display all information
+        void displayInfo() const {
+            cout << "Name: " << name << ", Age: " << age << ", Student ID: " << studentID << endl;
+            displaySports();
+            displayAcademics();
+        }
+    };
 
 int main() {
     //Create a StudentAthlete instance
